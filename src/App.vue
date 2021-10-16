37ed1d049f217882724b24228299b7068fcac73f
Vic Shóstak <vic@shostak.dev>
2021-10-16 17:27:08 +0300

    Fix token renew in App component

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ import { defineComponent, onMounted, onUnmounted } from 'vue'
 import { useRouter } from 'vue-router'
 import { useStore } from '__/store'
 import { UPDATE_JWT, UPDATE_CURRENT_USER } from '__/store-constants'
-import { TokenDataService, TokenResponse } from '__/services'
+import { TokenDataService as Token, TokenResponse } from '__/services'
 import { Sidebar } from '__/components'
 
 export default defineComponent({
@@ -26,19 +26,21 @@ export default defineComponent({
 
     // Define needed variables.
     const { access_token, expire } = store.state.jwt
-    const { name: current_route } = router.currentRoute.value // get current route name
 
     // Define function for renew token.
     const tokenRenew = async () => {
       try {
-        const { data: token_response }: TokenResponse = await TokenDataService.renew(access_token)
+        const { data: token_response }: TokenResponse = await Token.renew(access_token)
         // Successful response from Auth server.
         if (token_response.status === 200) {
           // Store the token data:
           store.commit(UPDATE_JWT, token_response.jwt) // add JWT data to store
           store.commit(UPDATE_CURRENT_USER, token_response.user) // add user data to store
+          // Add a random string to the local storage to indicate that the user is authenticated.
+          localStorage.setItem('_komentory', Math.random().toString(36).substring(2, 36))
         } else if (token_response.status === 401) {
           // Failed response from Auth server.
+          const { name: current_route } = router.currentRoute.value // get current route name
           // Skip redirect, if current route name is 'register'.
           if (current_route !== 'register') router.push({ name: 'login' }) // 401: push User Login page
         } else console.warn(token_response.msg) // or show error message
@@ -51,7 +53,7 @@ export default defineComponent({
     const tokenRenewTimer = setInterval(async () => {
       let now = new Date() // standard now time in milliseconds
       let expire_time = new Date(expire * 1000 - 60000) // subtract 1 minute from expire
-      if (expire_time <= now) await tokenRenew() // if expire time is less or equal than now, renew token
+      if (expire_time <= now && localStorage.getItem('_komentory')) await tokenRenew() // if expire time is less or equal than now, renew token
     }, 60000) // 1 minute interval
 
     // If token and expire time not set, try to renew.
