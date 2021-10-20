44a87e18fc7b2f8ea912d4193d8e78c06fccc54b
Vic Shóstak <vic@shostak.dev>
2021-10-20 20:53:07 +0300

    Fix Project component

--- src/views/Project.vue
+++ src/views/Project.vue
@@ -1,19 +1,31 @@
 <template>
-  <h1>Project ID: {{ id }}</h1>
-  <div v-if="isLoading">
-    <ContentLoader />
-  </div>
-  <div v-else>
-    <p>
-      <router-link :to="{ name: 'projects' }">Back</router-link>
-    </p>
-    <p>{{ project.attrs.title }}</p>
-    <p>{{ project.created_at }}</p>
-    <p>Author: {{ project.author.first_name }}</p>
-    <p>Tasks: {{ project.tasks_count }}</p>
-    <div v-for="task in tasks" :key="task.id" class="mt-2 mb-2 py-2 px-3 border rounded">
-      <router-link :to="{ name: 'task-details', params: { id: task.id } }">{{ task.name }}</router-link> &mdash;
-      {{ task.description }}
+  <div class="grid grid-cols-1 sm:grid-cols-12 xl:grid-cols-24">
+    <div>
+      <Sidebar />
+    </div>
+    <div class="sm:col-span-11 xl:col-span-23">
+      <div class="border-b py-4 px-2 sm:py-6 sm:px-6">
+        <div class="inline-flex items-center space-x-4">
+          <router-link :to="{ name: 'projects' }" class="border-0 hover:text-main-light">
+            <ArrowLeftIcon class="h-7 w-7" />
+          </router-link>
+          <h1>{{ project.title }}</h1>
+        </div>
+      </div>
+      <div class="py-4 px-2 sm:py-6 sm:px-6">
+        <div v-if="isLoading">
+          <ContentLoader />
+        </div>
+        <div v-else>
+          <p>{{ project.created_at }}</p>
+          <p>Author: {{ author.first_name }}</p>
+          <h2>Tasks: {{ project.tasks_count }}</h2>
+          <div v-for="task in tasks" :key="task.id" class="mt-2 mb-2 py-2 px-3 border rounded">
+            <router-link :to="{ name: 'task-details', params: { id: task.id } }"> {{ task.name }}</router-link> &mdash;
+            {{ task.description }}
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -21,8 +33,9 @@
 <script lang="ts">
 import { defineComponent, onMounted, ref } from 'vue'
 import { useRouter } from 'vue-router'
+import { ArrowLeftIcon } from '@heroicons/vue/outline'
 import { ProjectDataService as Project, ProjectResponse } from '__/services'
-import { ContentLoader } from '__/components'
+import { ContentLoader, Sidebar } from '__/components'
 
 export default defineComponent({
   name: 'Project',
@@ -31,6 +44,8 @@ export default defineComponent({
   },
   components: {
     ContentLoader,
+    Sidebar,
+    ArrowLeftIcon,
   },
   setup: (props) => {
     // Define needed instances.
@@ -39,6 +54,7 @@ export default defineComponent({
     // Define needed variables.
     const isLoading = ref(true)
     const project: any = ref({})
+    const author: any = ref({})
     const tasks: any = ref([{}])
 
     // Define function for getting project by ID.
@@ -48,8 +64,14 @@ export default defineComponent({
         // Successful response from API server, or failed with warning message.
         if (project_response.status === 200) {
           // Get the project data:
-          project.value = project_response.project // add project info
-          tasks.value = project_response.project.tasks! // add project tasks to array
+          project.value = {
+            created_at: project_response.project.created_at, // add project created at date
+            updated_at: project_response.project.updated_at, // add project updated at date
+            tasks_count: project_response.project.tasks_count, // add project tasks count
+            ...project_response.project.attrs, // add project attributes
+          }
+          author.value = project_response.project.author // add project author
+          tasks.value = project_response.project.tasks // add project tasks
           // Cancel content loader.
           isLoading.value = false
         } else if (project_response.status === 404) {
@@ -65,7 +87,7 @@ export default defineComponent({
     onMounted(() => getProjectByID())
 
     // Return instances and lifecycle hooks.
-    return { project, tasks, isLoading }
+    return { project, author, tasks, isLoading }
   },
 })
 </script>
